155e49cebc9f9fe6e3627486babf1cb5cd0d5678
Cyril FERRER <cyril.ferrer31@gmail.com>
2025-01-14 23:11:03 +0100

    html lore section injected

--- js/pageFaction.js
+++ js/pageFaction.js
@@ -5,3 +5,19 @@ const storedFaction = JSON.parse(storedFactionString);
 // le bandeau affiche le nom de la Faction
 const headline = document.getElementsByClassName("headline");
 headline[0].innerText = storedFaction.name;
+
+// récupère les sections HTML pour injections futures d'enfants
+const presentationLorewise = document.querySelector(".presentationLorewise");
+const interaction = document.querySelector(".interaction");
+const presentationGamewise = document.querySelector(".presentationGamewise");
+
+// affiche le logo et le lore de la Faction
+const logoFaction = document.createElement("img");
+logoFaction.src=storedFaction.logo;
+presentationLorewise.appendChild(logoFaction);
+const loreFaction = document.createElement("p");
+loreFaction.innerText = storedFaction.description;
+presentationLorewise.appendChild(loreFaction);
+
+// affiche les règles spéciales et cartes d'unités de la Faction
+// ********* TODO
